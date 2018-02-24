dfd5cc8672c717e47082d8e61596f23194fa87f1
William Robertson <williamp.robertson@gmail.com>
2018-02-24 02:11:02 -0700

    initial framework

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+##  Seeds for MySQL
+
+INSERT INTO burgers (burger_name) VALUES ('Goliath');
+INSERT INTO burgers (burger_name) VALUES ('Banzai');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Rodeo Cheeseburger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Montana', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain Jane', true);
+INSERT INTO burgers (burger_name) VALUES ('BBQ Chicken');
